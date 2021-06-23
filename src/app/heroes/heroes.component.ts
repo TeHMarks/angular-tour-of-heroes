56b58609e9c8cce87ef08a5395b51a9531d52756
TeHMarks <tehmarks96@gmail.com>
2021-06-23 18:17:48 +0200

    Services addition

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
+import { MessageService } from '../message.service';
+
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  /* Initialize the hero property to be of type Hero with the following structure {id: number, name: string} */
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
-  heroes = HEROES;
-  constructor() { }
-
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
   ngOnInit() {
-  }
+    this.getHeroes();
+   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+  //Now getHeroes() doesn't return a []
+  //but returns an Observable<Heroes[]>
+  //so we have to handle it with subscribe()
+  //thi because the operation has to be asynchronous
+  getHeroes():void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes)
+  }
 }
